14cb8ca7b7038416f7ed3f4dc64a66047ca7b88d
salenakha <140116192+salenakha@users.noreply.github.com>
2025-06-19 19:51:46 -0400

    Update Main.java

--- Big Data/Main.java
+++ Big Data/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Algorithm test = new Algorithm();
         System.out.println(test.avCrashDur());
-        // System.out.println(test.avSev());
-        // System.out.println(test.worstState());
-        //System.out.println(test.worstTime());
+        System.out.println(test.avSev());
+        System.out.println(test.worstState());
+        System.out.println(test.worstTime());
     }
 }
